dc43b95277af6d5df3976d66949f09c3725e7e1d
Jonathan D. Simms <jsimms@twitter.com>
2017-12-21 13:49:49 -0500

    initial import

---
+++ src/slyphon/zshbackup/backup.py
@@ -0,0 +1,227 @@
+#!/usr/bin/env python2.7
+
+from __future__ import print_function
+
+import argparse
+import logging
+import logging.config
+import os
+import os.path
+import re
+import sqlite3
+import sys
+
+from collections import defaultdict
+from contextlib import contextmanager
+from logging import DEBUG, INFO
+from textwrap import dedent
+
+log = logging.getLogger(__name__)
+
+HOME = os.environ['HOME']
+DEFAULT_DB_LOCATION = os.path.join(HOME, '.zsh_hist_backup.db')
+ZSHHIST_PATH = os.path.join(HOME, '.zshhistory')
+TABLE_NAME = 'history'
+
+def utf8(x):
+  return unicode(x, 'UTF-8', 'replace')
+
+
+def mk_conn(path, *a):
+  conn = sqlite3.connect(path, *a)
+  conn.text_factory = sqlite3.OptimizedUnicode
+  conn.row_factory = sqlite3.Row
+  return conn
+
+
+@contextmanager
+def open_conn(path, *a):
+  conn = mk_conn(path, *a)
+  try:
+    yield conn
+  finally:
+    conn.close()
+
+
+def table_exists(conn):
+  cursor = conn.cursor()
+  cursor.execute(
+    "SELECT name from sqlite_master WHERE type='table' AND name='{0}'".format(TABLE_NAME)
+  )
+
+  return len(cursor.fetchall()) > 0
+
+
+def create_table(conn):
+  cursor = conn.cursor()
+  cursor.execute("""
+    CREATE TABLE {0} (
+      timestamp BIGINT,
+      counter INTEGER NOT NULL DEFAULT 0,
+      command text NOT NULL,
+      PRIMARY KEY (timestamp, counter)
+    )
+  """.format(TABLE_NAME))
+  conn.commit()
+  log.info("table created successfully")
+
+
+def init_db(conn):
+  with conn:
+    if not table_exists(conn):
+      create_table(conn)
+
+
+def count_rows(conn):
+  return conn.execute("select count(*) as c from history").fetchone()['c']
+
+
+_REGEX = re.compile(r"""^: (?P<ts>\d+):\d+;(?P<cmd>.*)$""")
+
+
+def history_iter(fp):
+  countdict = defaultdict(int)
+
+  for line in fp:
+    m = _REGEX.match(line)
+    if m:
+      ts = int(m.group('ts'))
+      cmd = utf8(m.group('cmd'))
+
+      d = dict(timestamp=ts, command=cmd, counter=0)
+
+      if ts in countdict:
+        countdict[ts] += 1
+        d['counter'] = countdict[ts]
+
+      yield d
+    else:
+      log.debug("BAD LINE: %r", line)
+
+
+def backup(conn, zshhist_fp):
+
+  initial_count = count_rows(conn)
+
+  with conn:
+    cur = conn.cursor()
+
+    q = u"""\
+      REPLACE INTO {table} ('timestamp', 'counter', 'command')
+        VALUES(:timestamp, :counter, :command)
+    """.format(table=TABLE_NAME)
+
+    cur.executemany(q, history_iter(zshhist_fp))
+
+  log.info("inserted {0} rows".format(count_rows(conn)- initial_count))
+
+
+def cmd_backup(req):
+  with open_conn(req.dbpath) as conn:
+    init_db(conn)
+    with open(req.histfile) as fp:
+      backup(conn, fp)
+  log.info("complete!")
+
+
+BOGO_TS = 1478541623
+
+
+def cmd_munge(req):
+  """fixes historical data where my timestamp was the same for 3 years :P"""
+  with open_conn(req.dbpath) as conn:
+    init_db(conn)
+    nrows = conn.execute("select count(*) from history where timestamp = ?",
+        (BOGO_TS,)).fetchone()[0]
+
+    ts = BOGO_TS - nrows
+
+    for row in conn.execute("select command from history where timestamp = ?", (BOGO_TS,)):
+      print(": {ts}:0;{cmd}".format(ts=ts, cmd=row[0].encode('utf-8')), file=req.output)
+      ts += 1
+
+
+def cmd_restore(req):
+  with open_conn(req.dbpath) as conn:
+    init_db(conn)
+    for row in conn.execute("select timestamp, command from history"):
+      print(": {ts}:0;{cmd}".format(ts=ts, cmd=row[0].encode('utf-8')), file=req.output)
+
+
+def cmd_stats(req):
+  with open_conn(req.dbpath) as conn:
+    init_db(conn)
+
+
+def logging_setup(level):
+  logging.config.dictConfig({
+    'version': 1,
+    'disable_existing_loggers': False,  # this fixes the problem
+    'formatters': {
+      'standard': {
+        'format': '%(asctime)s [%(levelname)s] %(name)s: %(message)s'
+      },
+    },
+    'handlers': {
+      'default': {
+        'level': level,
+        'class': 'logging.StreamHandler',
+      },
+    },
+    'loggers': {
+      '': {
+        'handlers': ['default'],
+        'level': level,
+        'propagate': True
+      },
+      'twitter.git': {'level': 'CRITICAL'},
+      'requests_kerberos': {'level': 'CRITICAL'}
+    }
+  })
+
+
+
+def main():
+  parser = argparse.ArgumentParser(prog='zsh-backup')
+
+  parser.add_argument(
+      '-p' '--zsh-hist-path', dest='histfile',
+      help='path to .zshhist file',
+      default=ZSHHIST_PATH)
+
+  parser.add_argument(
+      '-d', '--dbpath', dest='dbpath',
+      help='path to the sqlite db file',
+      default=DEFAULT_DB_LOCATION)
+
+  parser.add_argument(
+    '--log-level', dest='log_lvl',
+    choices="DEBUG INFO WARN ERROR FATAL".split(' '),
+    help='set logging level',
+    default='INFO'
+  )
+
+
+  sub = parser.add_subparsers()
+  backup_p = sub.add_parser('backup')
+  backup_p.set_defaults(func=cmd_backup)
+
+  munge_p = sub.add_parser('munge')
+  munge_p.set_defaults(func=cmd_munge)
+  munge_p.add_argument("output", type=argparse.FileType('wb'), default='-')
+
+  restore_p = sub.add_parser('restore')
+  restore_p.set_defaults(func=cmd_restore)
+  restore_p.add_argument("output", type=argparse.FileType('wb'), default='-')
+
+  stats_p = sub.add_parser('stats')
+  stats_p.set_defaults(func=cmd_stats)
+
+  parsed = parser.parse_args()
+  logging_setup(parsed.log_lvl)
+
+  parsed.func(parsed)
+
+
+if __name__ == '__main__':
+  main()

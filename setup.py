986f476a5ae38d8a782857c563107ed7a5d97763
Jonathan D. Simms <jsimms@twitter.com>
2017-12-23 16:00:25 -0500

    add requirements in setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+from setuptools import setup
 
 VERSION='0.0.1' # @@VERSION@@
 
@@ -12,6 +12,11 @@ setup(
   author_email='jds@slyphon.com',
   url='https://github.com/slyphon/zsh-history-backup',
   packages = ['zshbackup'],
-  python_requires='>2.7, <=3.7'
+  python_requires='>2.7, <=3.7',
+  install_requires=[
+    'arrow>=0.12.0,<1.0'
+    'python-dateutil>=2.6.1,<3.0',
+    'six>=1.11.0,<2',
+    'backports.functools-lru-cache>=1.4,<2.0',
+  ]
 )
-

c62723d60f04eae6b54859b0f198c88e8cca9e12
Jonathan D. Simms <jsimms@twitter.com>
2017-12-24 17:23:21 -0500

    use a top-level src directory

--- setup.py
+++ setup.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 
-from setuptools import setup
+from glob import glob
+from os.path import basename, dirname, join, splitext
+
+from setuptools import setup, find_packages
 
 VERSION='0.0.1' # @@VERSION@@
 
@@ -10,13 +13,20 @@ setup(
   description='backup your zshhistory to a sqlite3 db',
   author='Jonathan Simms',
   author_email='jds@slyphon.com',
-  url='https://github.com/slyphon/zsh-history-backup',
-  packages = ['schist'],
+  url='https://github.com/slyphon/schist',
+  packages=find_packages('src', exclude=['tests']),
+  package_dir={'': 'src'},
+  py_modules=[splitext(basename(path))[0] for path in glob('src/*.py')],
   python_requires='>2.7, <=3.7',
   install_requires=[
     'arrow>=0.12.0,<1.0'
     'python-dateutil>=2.6.1,<3.0',
     'six>=1.11.0,<2',
     'backports.functools-lru-cache>=1.4,<2.0',
-  ]
+  ],
+  entry_points={
+    'console_scripts': [
+      'schist=schist:app'
+    ]
+  }
 )
